7af0d08e42ec7af57ce9880776d57c19f5d17a25
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-03 16:38:19 -0300

    Custom Validation

---
+++ backend/src/main/java/com/gpcr/demo/resourcers/exceptions/ValidationError.java
@@ -0,0 +1,19 @@
+package com.gpcr.demo.resourcers.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public List<FieldMessage> getErros() {
+		return errors;
+	}
+	
+	public void addError(String fildName, String message) {
+		errors.add(new FieldMessage(fildName, message));
+	}
+	
+}
